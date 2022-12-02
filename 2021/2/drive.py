31582a2e54be89f68089e079f698abb3a24400f1
Lucas Vasconcelos <lucas@vscncls.xyz>
2022-12-02 17:53:42 -0300

    2021

---
+++ 2021/2/drive.py
@@ -0,0 +1,36 @@
+from enum import Enum
+
+class MoveCommand(Enum):
+    FORWARD = "forward"
+    DOWN = "down"
+    UP = "up"
+
+def move(command: MoveCommand, value: int, curr_pos: tuple[int, int]) -> tuple[int, int]:
+    if command == MoveCommand.FORWARD:
+        return curr_pos[0] + value, curr_pos[1]
+    elif command ==  MoveCommand.DOWN:
+        return curr_pos[0], curr_pos[1] + value
+    elif command ==  MoveCommand.UP:
+        return curr_pos[0], curr_pos[1] - value
+
+def read_file_to_strings() -> list[str]:
+    with open("input.txt") as file:
+        return file.readlines()
+
+def transform_input(entry: list[str]) -> list[tuple[MoveCommand, int]]:
+    result: list[tuple[MoveCommand, int]] = []
+    for i in entry:
+        command = i.split(" ")
+        result.append((MoveCommand(command[0]), int(command[1])))
+
+    return result
+
+def calculate_result(entry: list[tuple[MoveCommand, int]]) -> tuple[int, int]:
+    curr_pos = (0, 0)
+    for i in entry:
+        curr_pos = move(i[0], i[1], curr_pos)
+    return curr_pos
+
+if __name__ == "__main__":
+    final_pos = calculate_result(transform_input(read_file_to_strings()))
+    print(final_pos)
